11ecc9b8212a77d58ef56178386657694110614c
aliaksei6777 <aliaksei6777@gmail.com>
2021-03-08 00:06:00 +0300

    added useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import "./App.css"
 import Counter from './components/Counter';
 import CounterSetup from "./components/CounterSetup";
@@ -11,6 +11,39 @@ function App() {
     const [maxValue, setMaxValue] = useState<number>(5)
     const [startValue, setStartValue] = useState<number>(0)
     const [textMode,setTextMode] = useState<boolean>(false)
+
+    useEffect(() => {
+        let valueAsString = localStorage.getItem('counterValue')
+        if(valueAsString) {
+            let newValue = JSON.parse(valueAsString)
+            setCounterValue(newValue)
+        }
+    }, [])
+    useEffect(() => {
+        localStorage.setItem('counterValue',JSON.stringify(counterValue))
+    }, [counterValue])
+
+    useEffect(() => {
+        let valueAsString = localStorage.getItem('maxValue')
+        if(valueAsString) {
+            let newValue = JSON.parse(valueAsString)
+            setMaxValue(newValue)
+        }
+    }, [])
+    useEffect(() => {
+        localStorage.setItem('maxValue',JSON.stringify(maxValue))
+    }, [maxValue])
+
+    useEffect(() => {
+        let valueAsString = localStorage.getItem('startValue')
+        if(valueAsString) {
+            let newValue = JSON.parse(valueAsString)
+            setStartValue(newValue)
+        }
+    }, [])
+    useEffect(() => {
+        localStorage.setItem('startValue',JSON.stringify(startValue))
+    }, [startValue])
 
     const onIncClickHandler = () => setCounterValue(counterValue + 1)
     const onResetClickHandler = () => setCounterValue(startValue)
