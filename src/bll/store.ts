c2472335e687f1b1fe955bf299a823f103662425
aliaksei6777 <aliaksei6777@gmail.com>
2021-04-04 00:16:56 +0300

    +Redux

--- src/bll/store.ts
+++ src/bll/store.ts
@@ -4,6 +4,17 @@ import {reducer} from "./counter-reducer";
 const rootReducer = combineReducers({
     state: reducer
 })
+
+let preloadedState;
+const persistedTodostring = localStorage.getItem('app-state')
+if (persistedTodostring) {
+    preloadedState = JSON.parse(persistedTodostring)
+}
+
 export type AppRootStateType = ReturnType<typeof rootReducer>
 
-export const store = createStore(rootReducer)
+export const store = createStore(rootReducer,preloadedState)
+
+store.subscribe(() => {
+    localStorage.setItem('app-state',JSON.stringify(store.getState()))
+})
