2dc34f78b69928b9ed2be6f049e410454ea0c577
aliaksei6777 <aliaksei6777@gmail.com>
2021-04-15 15:16:27 +0300

    css adaptive fix

--- src/App.tsx
+++ src/App.tsx
@@ -9,28 +9,6 @@ import {AppRootStateType} from "./bll/store";
 function App() {
     const state = useSelector<AppRootStateType, InitialStateType>(state => state.state)
 
-    // useEffect(() => {
-    //     let counterValueAsString = localStorage.getItem('counterValue')
-    //     if (counterValueAsString) {
-    //         dispatch(setCounterValueAC(JSON.parse(counterValueAsString)))}
-    //     let maxValueAsString = localStorage.getItem('maxValue')
-    //     if (maxValueAsString) {
-    //         dispatch(setMaxValueAC(JSON.parse(maxValueAsString)))}
-    //     let startValueAsString = localStorage.getItem('startValue')
-    //     if (startValueAsString) {
-    //         dispatch(setStartValueAC(JSON.parse(startValueAsString)))}
-    //     let textModeAsString = localStorage.getItem('textMode')
-    //     if (textModeAsString) {
-    //         dispatch(setTextModeAC(JSON.parse(textModeAsString)))}
-    // }, [])
-    // useEffect(() => {
-    //     localStorage.setItem('counterValue', JSON.stringify(state.counterValue))
-    //     localStorage.setItem('maxValue', JSON.stringify(state.maxValue))
-    //     localStorage.setItem('startValue', JSON.stringify(state.startValue))
-    //     localStorage.setItem('textMode', JSON.stringify(state.textMode))
-    // }, [state.counterValue, state.maxValue,
-    //     state.startValue, state.textMode])
-
     return (
         <div className={"App"}>
             <div>
